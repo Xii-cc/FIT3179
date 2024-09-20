7fdfdcfead814d4c57ee1e6a240908b90dcf21af
(elie00131) <liewenxi@gmail.com>
2024-09-20 10:56:58 +0800

    Update Graticule for Week 9 Homework

---
+++ Homework_Week_9/homework_week_9.js
@@ -0,0 +1,4 @@
+var vg_1 = "dot_map.vg.json";
+vegaEmbed("#dot_map", vg_1).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
