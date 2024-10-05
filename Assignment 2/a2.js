93af354cd8564e7bf818cb580a1f99255504fbbf
(elie00131) <liewenxi@gmail.com>
2024-10-05 12:13:14 +0800

    added required interactions to proportional symbol map

--- Assignment 2/a2.js
+++ Assignment 2/a2.js
@@ -1,4 +1,24 @@
-var vg_1 = "dot_map.vg.json";
-vegaEmbed("#dot_map", vg_1).then(function(result) {
+var vg_1 = "rotate_globe.vg.json";
+vegaEmbed("#rotate_globe_vis", vg_1).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_2 = "bar_country_airport.vg.json";
+vegaEmbed("#bar_country_airport_vis", vg_2).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_3 = "proportional_symbol_map_mly.vg.json";
+vegaEmbed("#proportional_symbol_map_vis", vg_3).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_4 = "donut_chart.vg.json";
+vegaEmbed("#donut_chart_vis", vg_4).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_5 = "heat_map_mly_passenger.vg.json";
+vegaEmbed("#heat_map_vis", vg_5).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
